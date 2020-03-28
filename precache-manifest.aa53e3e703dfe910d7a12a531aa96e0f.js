26983741b6ed0a114c16bf5cd78620c29b711629
Paco Gomez Granados <pacogomezgranados@gmail.com>
2020-03-28 17:07:11 +0100

    Updates

---
+++ precache-manifest.aa53e3e703dfe910d7a12a531aa96e0f.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ccb11d44529bcd7f56ca38fa80ab7ba",
+    "url": "/pitxapie/index.html"
+  },
+  {
+    "revision": "9ff7aee07393d5249b3f",
+    "url": "/pitxapie/static/css/main.58808ba6.chunk.css"
+  },
+  {
+    "revision": "650e0c08b69e0d72b12e",
+    "url": "/pitxapie/static/js/2.fdce0180.chunk.js"
+  },
+  {
+    "revision": "24d971a89960b68a3ae1ae658ff82f22",
+    "url": "/pitxapie/static/js/2.fdce0180.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ff7aee07393d5249b3f",
+    "url": "/pitxapie/static/js/main.db707d6d.chunk.js"
+  },
+  {
+    "revision": "c9e2410ea97f8f2cb4ba",
+    "url": "/pitxapie/static/js/runtime-main.500ff3c9.js"
+  },
+  {
+    "revision": "812db292a99c7fd676597f734a53b3b6",
+    "url": "/pitxapie/static/media/boar.812db292.svg"
+  },
+  {
+    "revision": "4c04d0d8a003e9b0bc3465fc94ac9b9c",
+    "url": "/pitxapie/static/media/chick.4c04d0d8.svg"
+  },
+  {
+    "revision": "ac86d0fcb260c4621e5092d9e23646ed",
+    "url": "/pitxapie/static/media/chicken.ac86d0fc.svg"
+  },
+  {
+    "revision": "118894dadd2dae44f1eefd35726df9c5",
+    "url": "/pitxapie/static/media/cow.118894da.svg"
+  },
+  {
+    "revision": "47e7a89f5a083bc2395d5da3a285cbc4",
+    "url": "/pitxapie/static/media/crab.47e7a89f.svg"
+  },
+  {
+    "revision": "0d5b5d7301c5efb49c3e2ca872c319ef",
+    "url": "/pitxapie/static/media/crocodile.0d5b5d73.svg"
+  },
+  {
+    "revision": "5745b4baaef0cd776ec3af4e0d23517d",
+    "url": "/pitxapie/static/media/dog.5745b4ba.svg"
+  },
+  {
+    "revision": "6bf760fafc42d2e56e9ae6ce3bbe9a4b",
+    "url": "/pitxapie/static/media/elephant.6bf760fa.svg"
+  },
+  {
+    "revision": "9e588f4c761c31737e948c777dc807a5",
+    "url": "/pitxapie/static/media/fox.9e588f4c.svg"
+  },
+  {
+    "revision": "5947c8f5b99913d2aa2a42fe837d63ab",
+    "url": "/pitxapie/static/media/frog.5947c8f5.svg"
+  },
+  {
+    "revision": "5fe657a0c4ca646540beaa8e99935714",
+    "url": "/pitxapie/static/media/giraffe.5fe657a0.svg"
+  },
+  {
+    "revision": "a45cfdd01ea492d4955f45ed028c7ec7",
+    "url": "/pitxapie/static/media/goat.a45cfdd0.svg"
+  },
+  {
+    "revision": "99420a8e322b5c1eb3db54ba1eefefb1",
+    "url": "/pitxapie/static/media/lion.99420a8e.svg"
+  },
+  {
+    "revision": "ddde7cb028d3046e649b9e7a8de4bbd3",
+    "url": "/pitxapie/static/media/pig.ddde7cb0.svg"
+  },
+  {
+    "revision": "c5bf81f2df95ab94aad808c92e2c7ad3",
+    "url": "/pitxapie/static/media/rooster.c5bf81f2.svg"
+  },
+  {
+    "revision": "da86707b80e0dfde926e79dc1d365d46",
+    "url": "/pitxapie/static/media/sheep.da86707b.svg"
+  }
+]);
