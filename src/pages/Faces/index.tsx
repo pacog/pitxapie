c6ec2eac4b6504febc469b5e64e6a3cd5bd90c75
Paco Gomez Granados <pacogomezgranados@gmail.com>
2020-09-03 14:41:03 +0200

    Extract ColorChangeBG component

--- src/pages/Faces/index.tsx
+++ src/pages/Faces/index.tsx
@@ -1,24 +1,17 @@
 import React from "react";
 import DetectAnyInput from "components/DetectAnyInput";
-import { motion } from "framer-motion";
+import ColorChangeBG from "components/ColorChangeBG";
 import "./style.css";
 
 function Faces() {
     return (
         <DetectAnyInput onAnyInput={() => {}}>
-            <motion.div
-                animate={{
-                    backgroundColor: [
-                        // "#231942",
-                        "#5e548e",
-                        "#9f86c0",
-                        "#be95c4",
-                        // "#e0b1cb"
-                    ],
-                }}
-                transition={{ yoyo: Infinity, duration: 10, ease: "linear" }}
-                className="faces full-screen"
-            ></motion.div>
+            <ColorChangeBG
+                className="faces"
+                colorList={["#5e548e", "#9f86c0", "#be95c4"]}
+            >
+                Hola
+            </ColorChangeBG>
         </DetectAnyInput>
     );
 }
