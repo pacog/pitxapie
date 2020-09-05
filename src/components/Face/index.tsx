36ce483bb0aceb1e79a6f3cf99e0db4e5062c291
Paco Gomez Granados <pacogomezgranados@gmail.com>
2020-09-05 22:43:52 +0200

    More faces and showing them from different sides

--- src/components/Face/index.tsx
+++ src/components/Face/index.tsx
@@ -1,26 +1,82 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { motion } from "framer-motion";
-import Mama from "./mama.png";
+import mama from "./mama.png";
+import massimo from "./massimo.png";
+import abuelomiguel from "./abuelomiguel.png";
+import kakitrien from "./kakitrien.png";
+import lola from "./lola.png";
+import mark from "./mark.png";
+import lucas from "./lucas.png";
+import { getRandomFromArray } from "utils/random";
 import "./style.css";
 
 interface IFaceProps {
     onAnimationEnd: () => void;
 }
 
+const ALL_FACES = [mama, massimo, abuelomiguel, kakitrien, lola, mark, lucas];
+const ALTERNATIVE_POSITIONS = ["top", "left", "right", "bottom"];
+
 function Face({ onAnimationEnd }: IFaceProps) {
+    const face = useMemo(() => getRandomFromArray(ALL_FACES), []);
+    const position = useMemo(
+        () => getRandomFromArray(ALTERNATIVE_POSITIONS),
+        []
+    );
+    const animate = useMemo(() => {
+        switch (position) {
+            case "top":
+                return {
+                    y: ["-100%", "0%", "0%", "-100%"],
+                };
+            case "bottom":
+                return {
+                    y: ["100%", "0%", "0%", "100%"],
+                };
+            case "left":
+                return {
+                    x: ["-100%", "0%", "0%", "-100%"],
+                };
+            case "right":
+                return {
+                    x: ["100%", "0%", "0%", "100%"],
+                };
+        }
+    }, [position]);
+
+    const initial = useMemo(() => {
+        switch (position) {
+            case "top":
+                return {
+                    y: "-100%",
+                    rotate: "-180deg",
+                };
+            case "bottom":
+                return {
+                    y: "100%",
+                };
+            case "left":
+                return {
+                    x: "-100%",
+                    rotate: "90deg",
+                };
+            case "right":
+                return {
+                    x: "100%",
+                    rotate: "-90deg",
+                };
+        }
+    }, [position]);
+
     return (
         <motion.div
-            className="face"
-            animate={{
-                y: ["100%", "0%", "0%", "100%"],
-            }}
+            className={`face face-${position}`}
+            animate={animate}
             transition={{ duration: 2, ease: "easeInOut" }}
-            initial={{
-                y: "100%",
-            }}
+            initial={initial}
             onAnimationComplete={onAnimationEnd}
         >
-            <img src={Mama} alt={"Mama"} />
+            <img src={face} alt={""} />
         </motion.div>
     );
 }
