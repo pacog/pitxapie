0b9bf5d81f785ff0cb3f882deb047edab790f1e4
Paco Gomez Granados <pacogomezgranados@gmail.com>
2020-09-17 14:46:36 +0200

    Adding many faces

--- src/components/Face/index.tsx
+++ src/components/Face/index.tsx
@@ -1,12 +1,36 @@
 import React, { useMemo } from "react";
 import { motion } from "framer-motion";
+import aafke from "./aafke.png";
+import bisa from "./bisa.png";
+import inaki_aafke from "./inaki_aafke.png";
+import mama_1 from "./mama_1.png";
+import massimo_2 from "./massimo_2.png";
+import abuela_carmen from "./abuela_carmen.png";
+import carla from "./carla.png";
+import kakitrien from "./kakitrien.png";
+import mama_2 from "./mama_2.png";
+import massimo_alex from "./massimo_alex.png";
+import abuelo_jose from "./abuelo_jose.png";
+import carmen from "./carmen.png";
+import lola from "./lola.png";
+import manuel from "./manuel.png";
+import rum from "./rum.png";
+import abuelomiguel from "./abuelomiguel.png";
+import carmen_aritz from "./carmen_aritz.png";
+import lucas from "./lucas.png";
+import manuel_agustin from "./manuel_agustin.png";
+import rum_niki from "./rum_niki.png";
+import andreu_1 from "./andreu_1.png";
+import catrien_ana from "./catrien_ana.png";
+import maka from "./maka.png";
+import mark from "./mark.png";
+import tres_locos from "./tres_locos.png";
+import andreu_2 from "./andreu_2.png";
+import giovane from "./giovane.png";
 import mama from "./mama.png";
 import massimo from "./massimo.png";
-import abuelomiguel from "./abuelomiguel.png";
-import kakitrien from "./kakitrien.png";
-import lola from "./lola.png";
-import mark from "./mark.png";
-import lucas from "./lucas.png";
+import xabi from "./xabi.png";
+
 import {
     getRandomFromArray,
     getRandomFromArrayWithWeights,
@@ -17,7 +41,38 @@ interface IFaceProps {
     onAnimationEnd: () => void;
 }
 
-const ALL_FACES = [mama, massimo, abuelomiguel, kakitrien, lola, mark, lucas];
+const ALL_FACES = [
+    aafke,
+    bisa,
+    inaki_aafke,
+    mama_1,
+    massimo_2,
+    abuela_carmen,
+    carla,
+    kakitrien,
+    mama_2,
+    massimo_alex,
+    abuelo_jose,
+    carmen,
+    lola,
+    manuel,
+    rum,
+    abuelomiguel,
+    carmen_aritz,
+    lucas,
+    manuel_agustin,
+    rum_niki,
+    andreu_1,
+    catrien_ana,
+    maka,
+    mark,
+    tres_locos,
+    andreu_2,
+    giovane,
+    mama,
+    massimo,
+    xabi,
+];
 const ALTERNATIVE_POSITIONS = [
     {
         weight: 0.1,
