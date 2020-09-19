0baf596e0f07a41f99f84b43f687f0b40353d920
Paco Gomez Granados <pacogomezgranados@gmail.com>
2020-09-19 15:21:46 +0200

    Try fix for gh pages route

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import "./App.css";
 
 function App() {
     return (
-        <Router>
+        <Router basename={process.env.PUBLIC_URL}>
             <Switch>
                 <Route path="/faces">
                     <Faces />
