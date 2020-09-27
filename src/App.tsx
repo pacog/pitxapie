9303038cd7e7afb9fa838ad60f15ec440e59eb74
Paco Gomez Granados <pacogomezgranados@gmail.com>
2020-09-27 11:47:44 +0200

    Using hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import Animals from "pages/Animals";
 import Faces from "pages/Faces";
 
