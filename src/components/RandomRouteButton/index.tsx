43f94638018796523e544fdd5250271021fe6129
Paco Gomez Granados <pacogomezgranados@gmail.com>
2020-09-27 12:53:25 +0200

    Random route button does callback when pressed enough

--- src/components/RandomRouteButton/index.tsx
+++ src/components/RandomRouteButton/index.tsx
@@ -1,20 +1,25 @@
-import React from "react";
+import React, { useRef } from "react";
 import { motion } from "framer-motion";
 
 import "./style.css";
 
 const TIME_TO_CHANGE = 2; // s
 
-function RandomRouteButton() {
+function RandomRouteButton({
+    onRequestChange,
+}: {
+    onRequestChange: () => void;
+}) {
+    const currentTime = useRef<number>(0);
+
     const onStartTap = () => {
-        console.log("start");
+        currentTime.current = performance.now();
     };
     const onEndTap = () => {
-        console.log("end");
-    };
-
-    const onCancelTap = () => {
-        console.log("cancel");
+        const elapsedTime = (performance.now() - currentTime.current) / 1000;
+        if (elapsedTime >= TIME_TO_CHANGE) {
+            onRequestChange();
+        }
     };
 
     return (
@@ -22,12 +27,11 @@ function RandomRouteButton() {
             className="random-route-button"
             onTapStart={onStartTap}
             onTap={onEndTap}
-            onTapCancel={onCancelTap}
             initial={{ scale: 0.3, opacity: 0.1, backgroundColor: "#f48c06" }}
             whileTap={{
                 scale: 1,
                 opacity: 0.8,
-                backgroundColor: "white",
+                backgroundColor: "#fff",
                 transition: { duration: TIME_TO_CHANGE },
             }}
         ></motion.div>
