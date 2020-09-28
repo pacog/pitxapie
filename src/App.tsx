7f771be6c11925e7992ea6cde2724aaf4d3dd4de
Paco Gomez Granados <pacogomezgranados@gmail.com>
2020-09-28 21:56:27 +0200

    Log version

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import LocationChanger from "components/LocationChanger";
 import { routes } from "routes";
 import "./App.css";
 
 function App() {
+    useEffect(() => {
+        console.log(`Pitxapie ${process.env.REACT_APP_VERSION}`);
+    }, []);
+
     return (
         <Router>
             <Switch>
